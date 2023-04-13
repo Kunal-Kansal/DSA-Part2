476fccd8c1db83f1f75b3659e3e1c84888941333
Kunal-Kansal <1kunalkansal@gmail.com>
2023-04-14 01:55:45 +0530

    intersection of sorted array

--- InterviewBit/BitManipulation/countTotalSet.js
+++ InterviewBit/BitManipulation/countTotalSet.js
@@ -1,2 +1,15 @@
 A = 3
 
+
+count = 0
+
+for(let j = 1 ; j <= A ;j++){
+    let binary = (j >>> 0).toString(2)
+    for(let i=0 ;i<binary.length ;i++){
+        if(binary[i] == "1"){count++}
+    }
+
+}
+let ans = (count)% ((10**9) + 7)
+return ans
+console.log(ans)
