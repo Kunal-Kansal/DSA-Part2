633fc8303151eaa1f0f2208bf428cecfc8e2fa0a
Kunal-Kansal <1kunalkansal@gmail.com>
2023-04-14 02:11:17 +0530

    remove duplicates

---
+++ InterviewBit/TwoPointers/removeDuplicates.js
@@ -0,0 +1,20 @@
+A = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 ]
+
+let mp = new Set()
+for(let i = 0 ; i < A.length ;i++){
+    if(mp.has(A[i])){continue}
+    else{
+        mp.add(A[i])
+    }
+}
+// return mp.size
+// for(let i = 0 ; i < A.length ;i++){
+//     let nums = i
+//     if(A[i] == A[i+1]){
+//         A.splice(i,1)
+//         i = nums-1
+//     }
+// }
+
+// return A.length
+console.log(mp.size)
