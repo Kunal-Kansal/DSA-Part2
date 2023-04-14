c82d00d970fe0649886e2daa6bcb80f6162e7d45
Kunal-Kansal <1kunalkansal@gmail.com>
2023-04-14 23:51:56 +0530

    decimal to fraction

--- InterviewBit/TwoPointers/pairWithGivenDifference.js
+++ InterviewBit/TwoPointers/pairWithGivenDifference.js
@@ -2,16 +2,49 @@ A = [ -259, -825, 459, 825, 221, 870, 626, 934, 205, 783, 850, 398 ]
 B = -42
 A = A.sort((a, b) => a - b)
 let flag = false
-console.log(A)
-for (let i = 0; i < A.length; i++) {
-    for (let j = i + 1; j < A.length; j++) {
-        if (Math.abs(A[i] - A[j]) == B) {
-            flag = true
-            break
-        }
-        else { continue }
+// // console.log(A)
+// for (let i = 0; i < A.length; i++) {
+//     for (let j = i + 1; j < A.length; j++) {
+//         let ans = Math.abs(A[i] - A[j])
+//         if (ans == Math.abs(B)) {
+//             flag = true
+//             break
+//         }
+//         else { continue }
+//         // console.log(ans)
+//     }
+// }
+// // if(flag == true){return 1}
+// // else{return 0}
+// console.log(flag)
+let arr = []
+for(let i = 0 ; i < A.length-1 ;i++){
+    arr.push(A[i+1] - A[i])
+}
+for(let i = 0 ; i < arr.length ;i++){
+    if(arr[i] == Math.abs(B)){
+        flag = true
     }
 }
-// if(flag == true){return 1}
-// else{return 0}
+let sum = arr[0]
+let arr1 = []
+for(let i = 1 ; i< arr.length ;i++){
+    if(sum == Math.abs(B) && flag == false){
+        flag = true
+        break
+    }
+    sum += arr[i]
+    arr1.push(sum)
+}
+sum = arr[arr.length -1]
+let arr2 = []
+for(let i = arr.length -2 ;i>= 0 ;i--){
+    if(sum == Math.abs(B) && flag == false){
+        flag =true
+        break
+    }
+    sum += arr[i]
+    arr2.push(sum)
+}
+
 console.log(flag)
---
+++ decimalToFraction.js
@@ -0,0 +1,65 @@
+let A = '41.254&21'
+
+let str = ""
+let bar = ""
+let flag = false
+
+for(let i = 0 ; i < A.length ;i++){
+    if(A[i] == "&"){
+        flag = true
+        continue
+    }
+    if(flag == true){
+        bar += A[i]
+    }
+    if(flag == false){
+        str+= A[i]
+    }
+}
+
+bar = str + bar
+strsize = 0
+strflag = false
+barsize = 0
+barflag = false
+for(let i = 0 ; i < str.length ;i++){
+    if(str[i] == "."){
+        strflag = true
+        continue
+    }
+    if(strflag == true){
+        strsize++
+    }
+}
+for(let i = 0 ; i < bar.length ;i++){
+    if(bar[i] == "."){
+        barflag = true
+        continue
+    }
+    if(barflag == true){
+        barsize++
+    }
+}
+
+
+let strleng = 1
+let barleng = 1
+
+for(let i = 0 ; i < strsize ;i++){
+    strleng *= 10
+}
+for(let i = 0 ; i < barsize ;i++){
+    barleng *= 10
+}
+
+str = parseFloat(str) * strleng
+bar = parseFloat(bar) * barleng
+
+let string = bar-str
+let num = barleng-strleng
+let ans = ""
+
+ans += string
+ans += " / "
+ans += num
+console.log(ans)
